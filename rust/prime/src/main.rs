dd34d8087bf308325dcf130271a673e19ab64636
Donald Ball <donald.ball@tanium.com>
2023-01-16 22:53:34 -0500

    add debug logs, delimit response, flush write

--- rust/prime/src/main.rs
+++ rust/prime/src/main.rs
@@ -8,9 +8,10 @@ async fn main() {
     loop {
         let (socket, _) = listener.accept().await.unwrap();
         tokio::spawn(async move {
+            eprintln!("accepted");
             match process(socket).await {
-                Ok(_) => eprintln!("success"),
-                Err(e) => eprintln!("error {}", e),
+                Ok(_) => eprintln!("completed normally"),
+                Err(e) => eprintln!("completed with error {:?}", e),
             }
         });
     }
@@ -34,41 +35,56 @@ async fn process(mut socket: TcpStream) -> io::Result<()> {
     let mut bufreader = io::BufReader::new(reader);
     loop {
         match bufreader.read_until(b'\n', &mut buffer).await {
-            Ok(0) if buffer.is_empty() => break,
+            Ok(0) if buffer.is_empty() => {
+                eprintln!("client closed");
+                break
+            },
             Ok(0) => {
-                writer.write_all(b"{\"error\": true}").await.unwrap();
+                eprintln!("client closed with partial message!");
+                writer.write_all(b"{\"error\": true}\n").await.unwrap();
                 break;
             }
-            Ok(_) if buffer.last() != Some(&b'\n') => continue,
+            Ok(_) if buffer.last() != Some(&b'\n') => {
+                eprintln!("read_until must have been interrupted...?");
+                continue
+            }
             Ok(_) => {
                 buffer.pop();
                 match serde_json::from_slice::<Request>(&buffer) {
-                    Ok(req) if req.method == "prime" => {
+                    Ok(req) if req.method == "isPrime" => {
+                        eprintln!("read isPrime message! {}", req.number);
                         let mut prime = false;
                         if req.number.is_finite() && req.number > 0.0 && req.number.fract() == 0.0 && req.number < u64::MAX as f64 {
                             prime = primes::is_prime(req.number as u64);
                         }
                         let res = Response { method: req.method, prime };
-                        let data = serde_json::to_vec(&res).unwrap();
+                        let mut data = serde_json::to_vec(&res).unwrap();
+                        data.push(10);
                         if let Err(e) = writer.write_all(&data[..]).await { 
                             eprintln!("write error {:?}", e);
                             // Close the socket?
                             return Err(e);
                         }
+                        eprintln!("wrote response {}", prime);
+                        writer.flush().await?
                     },
                     _ => {
-                        writer.write_all(b"{\"error\": true}").await.unwrap();
+                        eprintln!("read some busted message");
+                        writer.write_all(b"{\"error\": true}\n").await.unwrap();
                         break
                     },
                 }
                 buffer.clear();
             }
             Err(e) => {
+                eprintln!("read error {:?}", e);
                 // Close the socket?
                 return Err(e)
             }
         }
     }
+    eprintln!("closing socket");
     socket.shutdown().await?;
+    eprintln!("closed socket");
     Ok(())
 }
