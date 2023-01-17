f2ad0984235d03172b67a28a69e998d104cf2e6d
Donald Ball <donald.ball@tanium.com>
2023-01-16 21:08:05 -0500

    wire it all up

--- rust/prime/src/main.rs
+++ rust/prime/src/main.rs
@@ -1,4 +1,5 @@
-use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use serde::{Serialize, Deserialize};
+use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -15,22 +16,59 @@ async fn main() {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Request {
+    method: String,
+    number: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Response {
+    method: String,
+    prime: bool,
+}
+
 async fn process(mut socket: TcpStream) -> io::Result<()> {
-    let (mut reader, _) = socket.split();
-    let mut buffer = [0; 1024];
+    let (reader, mut writer) = socket.split();
+    let mut buffer = Vec::with_capacity(1024);
+    let mut bufreader = io::BufReader::new(reader);
     loop {
-        match reader.read(&mut buffer[..]).await {
-            Ok(n) if n > 0 => {
-                eprintln!("read {}", n)
-            }
-            Ok(_) => {
+        match bufreader.read_until(b'\n', &mut buffer).await {
+            Ok(0) if buffer.is_empty() => break,
+            Ok(0) => {
+                writer.write_all(b"{\"error\": true}").await.unwrap();
                 break;
             }
+            Ok(_) if buffer.last() != Some(&b'\n') => continue,
+            Ok(_) => {
+                buffer.pop();
+                match serde_json::from_slice::<Request>(&buffer) {
+                    Ok(req) if req.method == "prime" => {
+                        let mut prime = false;
+                        if req.number.is_finite() && req.number > 0.0 && req.number.fract() == 0.0 && req.number < u64::MAX as f64 {
+                            prime = primes::is_prime(req.number as u64);
+                        }
+                        let res = Response { method: req.method, prime };
+                        let data = serde_json::to_vec(&res).unwrap();
+                        if let Err(e) = writer.write_all(&data[..]).await { 
+                            eprintln!("write error {:?}", e);
+                            // Close the socket?
+                            return Err(e);
+                        }
+                    },
+                    _ => {
+                        writer.write_all(b"{\"error\": true}").await.unwrap();
+                        break
+                    },
+                }
+                buffer.clear();
+            }
             Err(e) => {
-                // Are we supposed to try to shut down the socket?
+                // Close the socket?
                 return Err(e)
             }
         }
     }
-    socket.shutdown().await
+    socket.shutdown().await?;
+    Ok(())
 }
