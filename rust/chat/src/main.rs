3df1632da58f544d0e283941709d0a829d5ef512
Donald Ball <donald.ball@tanium.com>
2023-01-21 15:26:06 -0500

    it all works woah

--- rust/chat/src/main.rs
+++ rust/chat/src/main.rs
@@ -1,22 +1,19 @@
 pub mod person;
 pub mod room;
-pub mod room2;
 
 use person::Person;
+use room::Room;
 use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use room::Room;
 
 #[tokio::main]
 async fn main() {
     let room = room::Room::new();
-    eprintln!("initial room is closed {}", room.cmd_sender.is_closed());
     let listener = TcpListener::bind("0.0.0.0:9000").await.unwrap();
     loop {
         let (socket, _) = listener.accept().await.unwrap();
         let room2 = room.clone();
         tokio::spawn(async move {
-            eprintln!("conn.accepted room closed {}", room2.cmd_sender.is_closed());
             match process(socket, room2).await {
                 Ok(_) => eprintln!("conn.completed"),
                 Err(e) => eprintln!("conn.errored {:?}", e),
@@ -29,60 +26,61 @@ async fn process(mut socket: TcpStream, room: Room) -> io::Result<()> {
     let (reader, mut writer) = socket.split();
     let mut bufreader = io::BufReader::new(reader);
     let mut buffer = Vec::with_capacity(1024);
-    writer.write(&format!("What is your name?\n").into_bytes()).await?;
+    writer
+        .write(&format!("What is your name?\n").into_bytes())
+        .await?;
     if bufreader.read_until(b'\n', &mut buffer).await? == 0 {
         return Ok(());
     }
     buffer.pop();
     let name = String::from_utf8_lossy(&buffer[..]).to_string();
+    buffer.clear();
     eprintln!("got name {}", name);
     match Person::new(name) {
-        Some(person) => {
-            eprintln!("got person {:?} room is closed {}", person, room.cmd_sender.is_closed());
-            match room.enter(person.clone()).await {
-                Some((greeting, mut endpoint)) => {
-                    eprintln!("entered {}", greeting);
-                    writer.write(&greeting.into_bytes()).await?;
-                    loop {
-                        tokio::select! {
-                            receipt = endpoint.receiver.recv() => {
-                                match receipt {
-                                    Ok(msg) => {
-                                        writer.write(&msg.into_bytes()).await?;
-                                    },
-                                    Err(e) => {
-                                        eprintln!("received error from endpoint {}", e);
+        Some(person) => match room.enter(person.clone()).await {
+            Some((greeting, mut endpoint)) => {
+                eprintln!("entered {}", greeting);
+                writer.write(&greeting.into_bytes()).await?;
+                loop {
+                    tokio::select! {
+                        receipt = endpoint.rx.recv() => {
+                            match receipt {
+                                Some(msg) => {
+                                    writer.write(&msg.into_bytes()).await?;
+                                },
+                                None => {
+                                    break;
+                                },
+                            }
+                        },
+                        result = bufreader.read_until(b'\n', &mut buffer) => {
+                            match result {
+                                Ok(0) => {
+                                    room.leave(person.clone()).await;
+                                    break;
+                                },
+                                Ok(_) => {
+                                    let msg = String::from_utf8_lossy(&buffer[..]).to_string();
+                                    eprintln!("read {} from socket", msg);
+                                    buffer.clear();
+                                    if endpoint.tx.send(msg).await.is_err() {
                                         break;
-                                    },
-                                }
-                            },
-                            result = bufreader.read_until(b'\n', &mut buffer) => {
-                                match result {
-                                    Ok(0) => {
-                                        room.leave(person.clone()).await;
-                                        break;
-                                    },
-                                    Ok(_) => {
-                                        let msg = String::from_utf8_lossy(&buffer[..]).to_string();
-                                        if endpoint.sender.send(msg).await.is_err() {
-                                            break;
-                                        }
-                                    },
-                                    Err(_) => {
-                                        room.leave(person.clone()).await;
-                                        break;
-                                    },
-                                }
-                            },
-                        }
+                                    }
+                                },
+                                Err(_) => {
+                                    room.leave(person.clone()).await;
+                                    break;
+                                },
+                            }
+                        },
                     }
-                },
-                None => {
-                    eprintln!("no love in the room");
-                },
+                }
+            }
+            None => {
+                eprintln!("no love in the room");
             }
         },
-        None => {},
+        None => {}
     }
     socket.shutdown().await?;
     Ok(())
--- rust/chat/src/room.rs
+++ rust/chat/src/room.rs
@@ -1,6 +1,5 @@
-use std::collections::BTreeSet;
+use std::collections::BTreeMap;
 
-use tokio::sync::broadcast;
 use tokio::sync::mpsc;
 use tokio::sync::oneshot;
 
@@ -12,6 +11,10 @@ pub enum Command {
         person: Person,
         res: oneshot::Sender<(Message, Endpoint)>,
     },
+    Say {
+        person: Person,
+        message: Message,
+    },
     Leave {
         person: Person,
     },
@@ -21,40 +24,28 @@ pub type Message = String;
 
 #[derive(Debug)]
 pub struct Endpoint {
-    pub sender: mpsc::Sender<Message>,
-    pub receiver: broadcast::Receiver<Message>,
+    pub tx: mpsc::Sender<Message>,
+    pub rx: mpsc::Receiver<Message>,
 }
 
 #[derive(Debug, Clone)]
 pub struct Room {
-    pub cmd_sender: mpsc::Sender<Command>,
+    pub cmd_tx: mpsc::Sender<Command>,
 }
 
 impl Room {
     pub fn new() -> Self {
-        eprintln!("constructing a room fr fr");
-        let (cmd_sender, cmd_receiver) = mpsc::channel::<Command>(1);
-        let (broadcast_sender, broadcast_receiver) = broadcast::channel::<Message>(16);
-        tokio::spawn(
-            Self::log_broadcasts(broadcast_receiver)
-        );
-        let rcv_cmd_sender = cmd_sender.clone();
-        eprintln!("about to spawn receiver fr fr");
-        tokio::spawn(
-            Self::receive_commands(rcv_cmd_sender, cmd_receiver, broadcast_sender)
-        );
-        eprintln!("yooo");
-        Self { cmd_sender }
+        let (cmd_tx, cmd_rx) = mpsc::channel(1);
+        let cmd_tx_clone = cmd_tx.clone();
+        tokio::spawn(Self::receive_commands(cmd_tx_clone, cmd_rx));
+        Self { cmd_tx }
     }
 
     pub async fn enter(&self, person: Person) -> Option<(Message, Endpoint)> {
-        let (sender, receiver) = oneshot::channel();
-        let cmd = Command::Join {
-            person,
-            res: sender,
-        };
-        match self.cmd_sender.send(cmd).await {
-            Ok(()) => match receiver.await {
+        let (tx, rx) = oneshot::channel();
+        let cmd = Command::Join { person, res: tx };
+        match self.cmd_tx.send(cmd).await {
+            Ok(()) => match rx.await {
                 Ok(receipt) => Some(receipt),
                 Err(e) => {
                     eprintln!("enter receive error {}", e);
@@ -70,91 +61,93 @@ impl Room {
 
     pub async fn leave(&self, person: Person) {
         let cmd = Command::Leave { person };
-        self.cmd_sender.send(cmd).await;
+        self.cmd_tx.send(cmd).await;
     }
 
-    async fn receive_commands(
-        cmd_sender: mpsc::Sender<Command>,
-        mut cmd_receiver: mpsc::Receiver<Command>,
-        broadcast_sender: broadcast::Sender<Message>,
-    ) {
-        eprintln!("should be listening to commands wtf");
-        let mut people: BTreeSet<Person> = BTreeSet::new();
+    async fn receive_commands(cmd_tx: mpsc::Sender<Command>, mut cmd_rx: mpsc::Receiver<Command>) {
+        let mut people: BTreeMap<Person, mpsc::Sender<Message>> = BTreeMap::new();
         loop {
-            eprintln!("receiving commands");
-            match cmd_receiver.recv().await {
+            match cmd_rx.recv().await {
                 Some(Command::Join { person, res }) => {
                     let msg = format!("* {} has entered the room\n", person.name);
-                    broadcast_sender.send(msg).unwrap();
-                    let (personal_sender, personal_receiver) = mpsc::channel(1);
-                    let personal_broadcast_receiver = broadcast_sender.subscribe();
-                    let endpoint = Endpoint {
-                        sender: personal_sender,
-                        receiver: personal_broadcast_receiver,
-                    };
+                    people.values().for_each(|msg_tx| {
+                        let tx_clone = msg_tx.clone();
+                        let msg_clone = msg.clone();
+                        tokio::spawn(async move {
+                            tx_clone.send(msg_clone).await;
+                        });
+                    });
+                    let (tx1, rx1) = mpsc::channel(1);
+                    let (tx2, rx2) = mpsc::channel(1);
+                    let ours = Endpoint { tx: tx1, rx: rx2 };
+                    let theirs = Endpoint { tx: tx2, rx: rx1 };
                     let names = people
-                        .iter()
-                        .map(|p| p.name.clone())
+                        .keys()
+                        .map(|person| person.name.clone())
                         .collect::<Vec<String>>()
                         .join(", ");
                     let msg = format!("* The room contains: {}\n", names);
-                    people.insert(person.clone());
-                    let personal_broadcast_sender = broadcast_sender.clone();
-                    let personal_cmd_sender = cmd_sender.clone();
-                    tokio::spawn(
-                        Self::receive_messages(
-                            person,
-                            personal_receiver,
-                            personal_cmd_sender,
-                            personal_broadcast_sender,
-                        )
-                    );
-                    res.send((msg, endpoint)).unwrap();
+                    if res.send((msg, theirs)).is_ok() {
+                        people.insert(person.clone(), ours.tx);
+                        tokio::spawn(Self::receive_messages(person, ours.rx, cmd_tx.clone()));
+                    }
                 }
+                Some(Command::Say { person, message }) => people
+                    .iter()
+                    .filter(|(them, _)| **them != person)
+                    .for_each(|(them, msg_tx)| {
+                        let msg_tx_clone = msg_tx.clone();
+                        eprintln!("say say {:?} from {:?}", message, person);
+                        let msg = format!("[{}] {}", person.name, message);
+                        tokio::spawn(async move {
+                            msg_tx_clone.send(msg).await;
+                        });
+                    }),
                 Some(Command::Leave { person }) => {
-                    people.remove(&person);
-                    let msg = format!("* {} has left the room\n", person.name);
-                    broadcast_sender.send(msg).unwrap();
+                    if people.remove(&person).is_some() {
+                        let msg = format!("* {} has left the room\n", person.name);
+                        // copied from 50
+                        people.values().for_each(|msg_tx| {
+                            let tx_clone = msg_tx.clone();
+                            let msg_clone = msg.clone();
+                            tokio::spawn(async move {
+                                tx_clone.send(msg_clone).await;
+                            });
+                        });
+                    }
                 }
                 None => {
                     break;
                 }
             }
         }
-        eprintln!("done listening to commands");
     }
 
     async fn receive_messages(
         person: Person,
-        mut personal_receiver: mpsc::Receiver<Message>,
-        cmd_sender: mpsc::Sender<Command>,
-        broadcast_sender: broadcast::Sender<Message>,
+        mut msg_rx: mpsc::Receiver<Message>,
+        cmd_tx: mpsc::Sender<Command>,
     ) {
         loop {
-            match personal_receiver.recv().await {
-                Some(msg) => {
-                    let msg = format!("[{}] {}\n", person.name, msg);
-                    broadcast_sender.send(msg);
-                    continue;
+            match msg_rx.recv().await {
+                Some(message) => {
+                    if cmd_tx
+                        .send(Command::Say {
+                            person: person.clone(),
+                            message,
+                        })
+                        .await
+                        .is_err()
+                    {
+                        break;
+                    }
                 }
                 None => {
-                    cmd_sender.send(Command::Leave { person }).await.unwrap();
-                    break;
-                }
-            }
-        }
-    }
-
-    async fn log_broadcasts(mut receiver: broadcast::Receiver<Message>) {
-        eprintln!("loooooop");
-        loop {
-            eprintln!("logging broadcasts");
-            match receiver.recv().await {
-                Ok(msg) => {
-                    eprintln!("room.broadcast {:?}", msg);
-                    continue;
-                }
-                Err(_) => {
+                    cmd_tx
+                        .send(Command::Leave {
+                            person: person.clone(),
+                        })
+                        .await;
                     break;
                 }
             }
