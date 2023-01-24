6d6bc5b7558ac1e2e6d745e223d5da7464a59fe3
Donald Ball <donald.ball@tanium.com>
2023-01-24 00:32:54 -0500

    simpler

--- rust/kv/src/main.rs
+++ rust/kv/src/main.rs
@@ -1,44 +1,49 @@
 use std::collections::HashMap;
+use std::io;
+use std::net::SocketAddr;
 use tokio::net::UdpSocket;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), io::Error> {
     let mut data: HashMap<Vec<u8>, Vec<u8>> = HashMap::new();
-    let version_key = b"version";
-    let version = b"version=0";
+    let version_key = b"version".to_vec();
+    data.insert(version_key.clone(), b"0".to_vec());
     loop {
-        let mut buf: [u8; 1000] = [0; 1000];
-        match UdpSocket::bind("0.0.0.0:9000").await {
-            Ok(socket) => match socket.recv_from(&mut buf).await {
-                Ok((1000, _)) => (),
-                Ok((len, addr)) => {
-                    let read = &mut buf[0..len];
-                    match read.iter().position(|b| b == &b'=') {
-                        Some(i) => {
-                            let (k, _) = read.split_at(i);
-                            if k != version_key {
-                                data.insert(k.to_vec(), read.to_vec());
-                            }
-                        }
-                        None if read == version_key => match socket.send(version).await {
-                            Ok(_) => (),
-                            Err(_) => (),
-                        },
-                        None => match data.get(read) {
-                            Some(v) => match socket.connect(addr).await {
-                                Ok(_) => match socket.send(&v[..]).await {
-                                    Ok(_) => (),
-                                    Err(_) => (),
-                                },
-                                Err(_) => (),
-                            },
-                            None => (),
-                        },
-                    }
+        let socket = UdpSocket::bind("0.0.0.0:9000").await?;
+        match read_command(&socket).await {
+            Ok(Some(Command::Get { key, addr })) => {
+                if let Some(value) = data.get(&key) {
+                    socket.send_to(&value[..], addr).await;
                 }
-                Err(_) => (),
-            },
-            Err(_) => (),
+            }
+            Ok(Some(Command::Set { key, value })) if key != version_key => {
+                data.insert(key, value);
+            }
+            _ => (),
+        }
+    }
+}
+
+enum Command {
+    Get { key: Vec<u8>, addr: SocketAddr },
+    Set { key: Vec<u8>, value: Vec<u8> },
+}
+
+async fn read_command(socket: &UdpSocket) -> Result<Option<Command>, io::Error> {
+    let mut buf: [u8; 1000] = [0; 1000];
+    match socket.recv_from(&mut buf).await? {
+        (1000, _) => Ok(None),
+        (len, addr) => {
+            let buf = &mut buf[..len];
+            if let Some(i) = buf.iter().position(|b| b == &b'=') {
+                let (key, value) = buf.split_at(i);
+                let key = key.to_vec();
+                let value = value.to_vec();
+                Ok(Some(Command::Set { key, value }))
+            } else {
+                let key = buf.to_vec();
+                Ok(Some(Command::Get { key, addr }))
+            }
         }
     }
 }
